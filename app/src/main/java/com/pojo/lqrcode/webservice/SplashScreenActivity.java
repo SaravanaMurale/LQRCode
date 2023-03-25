b0b0adf40b4bfe4463c84d5c852dc4e7c958cb31
murali <saravanamurali24@gmail.com>
2023-03-25 12:57:24 +0530

    QrScan Completed Commit

---
+++ app/src/main/java/com/pojo/lqrcode/webservice/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.pojo.lqrcode.webservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.pojo.lqrcode.MainActivity;
+import com.pojo.lqrcode.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },500);
+
+    }
+}
